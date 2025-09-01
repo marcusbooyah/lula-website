bd808684b45868d33e7f37e79a4c09f39932e0b1
Marcus Bowyer <marcus@euarctos.com>
2025-09-01 15:23:31 -0700

    refactor: update site URL and base path; enhance layout and contact page styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marcusbooyah.github.io',
-  base: 'lula-website',
+  site: 'https://lula.tattoo',
+  base: '',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
